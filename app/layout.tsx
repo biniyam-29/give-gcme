f4ff4070f704cfecc4ff7c3f52373394014881a3
Biniyam <bnmmarkos@gmail.com>
2025-06-09 17:12:38 +0300

    FIX: added a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,20 @@ export const metadata: Metadata = {
   description:
     "Built for the purpose or fundraising for great commission staff.",
   generator: "Biniyam",
+  icons: {
+    icon: [
+      {
+        url: '/favicon.png',
+        type: 'image/png',
+      }
+    ],
+    apple: [
+      {
+        url: '/favicon.png',
+        type: 'image/png',
+      }
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +29,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </head>
       <body>{children}</body>
     </html>
   );
